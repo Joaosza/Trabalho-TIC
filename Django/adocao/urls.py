c78916da31e003a3f320e296db33396643923e97
Aluno <joao12308@gmail.com>
2019-03-29 20:48:17 -0300

    Trabalho com Django para a aula de TIC.

---
+++ Django/adocao/urls.py
@@ -0,0 +1,13 @@
+# módulo do Django para criar urls
+from django.urls import path
+
+# Importe todas suas classes do views.py
+from .views import *
+
+urlpatterns = [
+    # path('caminho/da/url', ClasseLáDoView.as_view(), name="nomeDessaURL" )
+    path('', PaginaInicialView.as_view(), name="index"),
+    path('sobre/', SobreView.as_view(), name="sobre"),
+    path('cadastro/', CadastroView.as_view(), name="cadastro"),
+    path('contato/', ContatoView.as_view(), name="contato"),
+]
